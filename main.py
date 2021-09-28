b41d902097ace00f57867e52f0cc98217c3cd8d9
jjohnston94 <jjohnston1994@gmail.com>
2021-09-28 18:14:35 -0400

    a

--- main.py
+++ main.py
@@ -131,6 +131,7 @@ def mp(L,M,K):
   Yall = Ytest[0] + 1j*Ytest[1]
   Xall = Xtest[0] + 1j*Xtest[1]
   Nsamp = Xall.shape[0]
+  Nsamp = 2
   
   # print('SNR=', 10*np.log10(np.linalg.norm(A@X)**2/np.linalg.norm(noise)**2))
   res = []
@@ -212,12 +213,51 @@ def lam_tradeoff():
     i += 1
 
   plt.show()
+  
+def LMK():
+  M = 8
+  K = 3
+  figL, axL = plt.subplots()
+  i = 0
+  NMSE_L = []
+  Llist = [4,8,12,16,20]
+  for L in Llist:
+  # for L in [12]:
+    NMSE, lams1, lams2, LMK = mp(L, M, K)
+    NMSE_L.append(NMSE[0,0])
+  axL.plot(Llist, NMSE_L)
+  
+  L = 12
+  K = 3
+  i = 0
+  figM, axM = plt.subplots()
+  NMSE_M = []
+  Mlist = [4,8,12,16]
+  for M in Mlist:
+    NMSE, lams1, lams2, LMK = mp(L, M, K)
+    NMSE_M.append(NMSE[0,0])
+  axM.plot(Mlist, NMSE_M)
+  
+
+  M = 8
+  L = 12
+  i = 0
+  figK, axK = plt.subplots()
+  Klist = [3,4,5,6,7,8]
+  NMSE_K = []
+  for K in Klist:
+    NMSE, lams1, lams2, LMK = mp(L, M, K)
+    NMSE_K.append(NMSE[0,0])
+  axK.plot(Klist, NMSE_K)
+
+  plt.show()
 
 if __name__ == '__main__':
-  NMSE, lams1, lams2, LMK = mp(L=12,M=8,K=3)
+  # NMSE, lams1, lams2, LMK = mp(L=12,M=8,K=3)
+  LMK()
 
   # fig, ax = plt.subplots()
   # plot_nmse(ax, NMSE, lams1, lams2, LMK)
   # plt.show()
 
-  print(NMSE)
+  # print(NMSE)
