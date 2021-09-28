05525b89a420565cdf1cfd61af79b36d42d7c417
jjohnston94 <jjohnston1994@gmail.com>
2021-09-28 15:08:27 -0400

    a

--- main.py
+++ main.py
@@ -125,10 +125,10 @@ def mp(L,M,K):
   
   # print('SNR=', 10*np.log10(np.linalg.norm(A@X)**2/np.linalg.norm(noise)**2))
   res = []
-  Nlam1 = 5
+  Nlam1 = 3
   Nlam2 = 10
   lams1 = np.logspace(-3,-1, Nlam1)
-  lams2 = np.logspace(-3,-1, Nlam2)
+  lams2 = np.logspace(-3,0, Nlam2)
 
   p = problem(*(N,L,M,P,K,(M,1),channel_sparsity))
 
